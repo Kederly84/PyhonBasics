7b78638e05a3764dc2af829a2c9fd8a61e4a0542
Aleksandr Maksimov <amaksimov744@gmail.com>
2021-10-01 12:27:05 +0300

    Выполнены все 3 задания.

--- HomeWork/HomeWork1.py
+++ HomeWork/HomeWork1.py
@@ -1,11 +1,15 @@
 # задание №1
+
+# Запрос ввода от пользователя в секундах
 duration = int(input('Веди время в секундах'))
 
 days = duration // 86400  # 86400 - это количество секунд в сутках, вычисляем кол-во суток
 hours = (duration - days * 86400) // 3600  # 3600 количество секунд в часе, вычисляем кол-во часов
 minutes = (duration - days * 86400 - hours * 3600) // 60  # Вычисляем кол-во минут
-seconds = duration - days * 86400 - hours * 3600 - minutes * 60
+seconds = duration - days * 86400 - hours * 3600 - minutes * 60 # Вычисляем оставшиеся секунды
 
+# Вывод информации в зависимости от данных, введенных пользователем
+# без лишних сущностей
 if duration >= 86400:
     print('Вы ввели', days, 'дн', hours, 'час', minutes, 'мин', seconds, 'сек')
 elif 3600 <= duration < 86400:
@@ -15,26 +19,6 @@ elif 60 <= duration < 3600:
 else:
     print('Вы ввели', duration, 'сек')
 
-# задание №2
-
-numbers = list(range(1, 101, 2))
-print(numbers)
-# cube_numbers = []
-# seven_numbers = []
-# for i in range(len(numbers)):
-#     if numbers[i] % 2 != 0:
-#         number = numbers[i] ** 3
-#         cube_numbers.append(number)
-#
-# print(cube_numbers)
-# #numbr = 0
-#
-# for x in range(len(cube_numbers)):
-#     operand = list(str(cube_numbers[x]))
-#     for y in range(len(operand)):
-#         numbr = numbr + int(operand[y])
-#         seven_numbers.append(numbr)
-# print(seven_numbers)
 
 
 
---
+++ HomeWork/HomeWork2.py
@@ -0,0 +1,56 @@
+# задание №2
+
+# Объявление всех необходимых переменных и создание списков
+numbers = []
+seven_numbers = []
+counter = 0
+total_sum = 0
+seventeen_sum = 0
+
+# Создание списка из кубов чисел от 1 до 1000
+for i in list(range(1, 1001, 2)):
+    i = i ** 3
+    numbers.append(i)
+
+# Вывод списка
+print(numbers)
+
+# Создание списка чисел сумма цифр которых делится без остатка на 7
+for i in range(len(numbers)):
+    number = list(str((numbers[i])))
+    y = 0
+    summary = 0
+    while y < len(number):
+        summary += int(number[y])
+        y += 1
+        if y == len(number) and summary % 7 == 0:
+            seven_numbers.append(numbers[i])
+
+# Вывод списка чисел, где сумма цифр делится на 7 без остатка
+print(seven_numbers)
+
+# Вычисление суммы всех чисел, сумма цифр которых делится на 7 без остатка
+while counter < len(seven_numbers):
+    total_sum += seven_numbers[counter]
+    counter += 1
+
+# Вывод полученной суммы
+print(total_sum)
+
+# Расчет суммы чисел первоначального списка numbers, где каждый элемент списка увеличен на 17
+# без создания нового промежуточного списка
+for i in range(len(numbers)):
+    check = numbers[i] + 17
+    number = list(str((check)))
+    b = 0
+    summary = 0
+    while b < len(number):
+        summary += int(number[b])
+        b += 1
+        if b == len(number) and summary % 7 == 0:
+            seventeen_sum += check
+
+# Вывод суммы чисел первоначального массива, где каждое число увеличено на 17
+# и суммаа цифр данного числа кратна 7
+print(seventeen_sum)
+
