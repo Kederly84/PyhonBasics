6d129de1a0c58b6d1aa582c1cdae7f263a493d89
Aleksandr Maksimov <amaksimov744@gmail.com>
2021-10-02 01:11:19 +0300

    Выполнены все 3 задания. Обновил readme.txt Оформил файлы Исправил третье задание.

--- HomeWork/HomeWork3.py
+++ HomeWork/HomeWork3.py
@@ -7,12 +7,19 @@ percent = []
 for i in list(range(1, 101)):
     percent.append(i)
 
-# Построчный вывод информации на экран в соответствии с заданными условиями
-for i in range(len(percent) + 1):
-    if i == 1:
-        print(i, 'процент')
-    elif 1 < i < 5:
-        print(i, 'процента')
-    else:
-        print(i, 'процентов')
-
+for i in range(len(percent)):
+    number = list(str(percent[i]))
+    if i == 0:
+        print(percent[i], 'процент')
+    if 1 <= i < 4:
+        print(percent[i], 'процента')
+    if i in range(4, 19):
+        print(percent[i], 'процентов')
+    if number[-1] == '1' and percent[i] > 19:
+        print(percent[i], 'процент')
+    if number[-1] == '0' and percent[i] > 19:
+        print(percent[i], 'процентов')
+    if int(number[-1]) in range(2, 5) and percent[i] > 19:
+        print(percent[i], 'процента')
+    if int(number[-1]) in range(5, 10) and percent[i] > 19:
+        print(percent[i], 'процентов')
